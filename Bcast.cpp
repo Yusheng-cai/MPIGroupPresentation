b3ece68004bd0a13a11c860bb2258afb776f73c7
Yusheng Cai <cys9741@seas.upenn.edu>
2021-07-17 16:29:03 -0400

    Minor changes to Bcast.cpp

--- Bcast.cpp
+++ Bcast.cpp
@@ -1,30 +1,44 @@
 #include <iostream>
+#include <sstream>
 #include <mpi.h>
 
 
 int main(int argc, char** argv)
 {
     MPI_Init(&argc, &argv);
-
+    int root=0;
     int rank;
     int size;
 
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    int data;
-    int root=0;
 
+
+    int data_size;
+    if (rank == root)
+    {
+        std::stringstream ss(argv[1]);
+        ss >> data_size;
+    }
+
+    MPI_Bcast(&data_size, 1, MPI_INT, root, MPI_COMM_WORLD);
+    std::cout << "This is processor " << rank << ". The data size is " << data_size << std::endl;
+
+    int data[data_size];
 
     // set the data to non-empty if we are on root
     if ( rank == root )
     {
-        data = 1;
+        for (int i=0; i< data_size;i++)
+        {
+            data[i] = 1;
+        }
     }
 
     // std::cout << "Before bcast, data = " << data << std::endl;
 
-    MPI_Bcast(&data, 1, MPI_INT, root, MPI_COMM_WORLD);
+    MPI_Bcast(&data, data_size, MPI_INT, root, MPI_COMM_WORLD);
 
     // std::cout << "After bcast, processor = " << rank << ", data = " << data << std::endl;
 
